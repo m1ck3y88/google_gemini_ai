fca1b69c349605ba7ddb18ab4706601d04b61f8c
Michael Pollard <mickeymakesapps88@gmail.com>
2024-07-26 14:11:24 -0700

    setup environment

---
+++ main.py
@@ -0,0 +1,8 @@
+from env.api_key import api_key
+import google.generativeai as genai
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+print("Google Generative AI Setup Completed Successfully!")
