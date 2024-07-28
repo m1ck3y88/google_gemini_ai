072896b89a35f854b1f159fac790d528eddcb08e
Michael Pollard <mickeymakesapps88@gmail.com>
2024-07-28 13:13:51 -0700

    create simple app that allows chat with Google's Gemini 1.5 Flash model

--- main.py
+++ main.py
@@ -7,3 +7,40 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 print("Google Generative AI Setup Completed Successfully!")
 
+
+def get_response(prompt):
+    response = model.generate_content(prompt)
+    return response.text
+
+
+def chat():
+    try:
+        chatting = True
+        prompt = input('What would you like to know?\nType "quit" to quit.\n').lower()
+
+        while chatting:
+
+            if prompt != 'quit':
+
+                print(get_response(prompt))
+                keep_going = input('Anything else? (y/n)\n').lower()
+
+                if keep_going in ['y', 'yes']:
+
+                    prompt = input('What would you like to know?\nType "quit" to quit.\n').lower()
+
+                elif keep_going in ['n', 'no', 'quit']:
+
+                    chatting = False
+                    print('Goodbye!')
+
+            else:
+                chatting = False
+                print('Goodbye!')
+
+    except Exception as e:
+        print('Invalid input. Please try again.')
+        print('Error: {}'.format(e))
+
+
+chat()
